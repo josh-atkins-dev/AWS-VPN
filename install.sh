72559b706266da284bc6bd273db25b89faa06dd1
Josh Atkins <josh.atkins.dev@gmail.com>
2017-03-21 16:19:41 +0000

    Add dynamic inventory

--- install.sh
+++ install.sh
@@ -2,3 +2,6 @@
 set -o errexit
 
 ansible-galaxy install angstwad.docker_ubuntu
+
+wget https://raw.githubusercontent.com/ansible/ansible/devel/contrib/inventory/ec2.py
+wget https://raw.githubusercontent.com/ansible/ansible/devel/contrib/inventory/ec2.ini
