0593280ba2b336bdd5b1d7bbcfc5257ef28dc738
Josh Atkins <josh.atkins.dev@gmail.com>
2017-03-21 16:25:50 +0000

    ec2.py is executable

--- install.sh
+++ install.sh
@@ -6,3 +6,4 @@ ansible-galaxy install angstwad.docker_ubuntu
 echo "Update dynamic inventory"
 wget -q https://raw.githubusercontent.com/ansible/ansible/devel/contrib/inventory/ec2.py -O ec2.py
 wget -q https://raw.githubusercontent.com/ansible/ansible/devel/contrib/inventory/ec2.ini -O ec2.ini
+chmox +x ./ec2.py
