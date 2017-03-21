6cb1885e7065e4ed93493e430952f4cfecf23b9b
Josh Atkins <josh.atkins.dev@gmail.com>
2017-03-21 16:59:32 +0000

    Add private key file location

--- init.sh
+++ init.sh
@@ -1,3 +1,5 @@
 __dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-alias vpn_is_present="ansible-playbook -i ${__dir}/hosts ${__dir}/aws_vpn.yml"
+alias vpn_is_present="ansible-playbook -i ${__dir}/ec2.py ${__dir}/aws_vpn.yml"
+
+export ANSIBLE_PRIVATE_KEY_FILE=${VM_TEMP_PATH}/ssh/${AWS_PREFIX}-key.pem
