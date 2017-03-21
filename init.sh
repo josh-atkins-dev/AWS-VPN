27f8fa8467d83ee395c22049c89154d3063e6dc4
Josh Atkins <josh.atkins.dev@gmail.com>
2017-03-21 17:35:49 +0000

    Connect to VPN

--- init.sh
+++ init.sh
@@ -3,4 +3,6 @@ __dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 alias vpn_is_present="ansible-playbook -i ${__dir}/ec2.py ${__dir}/aws_vpn.yml"
 alias update_security_group="ansible-playbook -i ${__dir}/hosts ${__dir}/update_security_group.yml"
 
+update_security_group
 export ANSIBLE_PRIVATE_KEY_FILE=${VM_TEMP_PATH}/ssh/${AWS_PREFIX}-key.pem
+export ANSIBLE_REMOTE_USER=ubuntu
