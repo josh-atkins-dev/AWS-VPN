30fbe1fec1a6660f0944e17511bd74c6f47f9222
Josh Atkins <josh.atkins.dev@gmail.com>
2017-03-22 10:06:08 +0000

    Add shortcut to ssh to vpn

--- init.sh
+++ init.sh
@@ -3,5 +3,9 @@ __dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 alias vpn_is_present="ansible-playbook -i ${__dir}/ec2.py ${__dir}/aws_vpn.yml"
 alias update_security_group="ansible-playbook -i ${__dir}/hosts ${__dir}/update_security_group.yml"
 
+
 export ANSIBLE_PRIVATE_KEY_FILE=${VM_TEMP_PATH}/ssh/${AWS_PREFIX}-key.pem
 export ANSIBLE_REMOTE_USER=ubuntu
+
+
+alias ssh_vpn="ssh -v -i ${VM_TEMP_PATH}/ssh/${AWS_PREFIX}-key.pem"
