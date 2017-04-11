211cfab86ff1b60ce76136b32e2c3b95a4275beb
josh-atkins-dev <josh.atkins.dev@gmail.com>
2017-04-11 04:08:12 +0100

    add openvpn role

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 set -o errexit
 
 ansible-galaxy install angstwad.docker_ubuntu
+ansible-galaxy install kbrebanov.openvpn
 
 echo "Update dynamic inventory"
 wget -q https://raw.githubusercontent.com/ansible/ansible/devel/contrib/inventory/ec2.py -O ec2.py
